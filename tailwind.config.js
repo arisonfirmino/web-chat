e3db6f4ee5abab52e6ee0e68cb076f64d303e474
Arison Firmino <arisonfirmino@hotmail.com>
2023-12-01 21:52:18 -0300

    chore: tailwind configured

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "bg-color": "#574a68",
+        "title-color": "#be29ec",
+        "text-color": "#efbbff",
+      },
+    },
+  },
+  plugins: [],
+};
